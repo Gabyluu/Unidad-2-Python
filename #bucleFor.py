307496200393f767b462622682af26083c950076
Gabyluu <42919324+Gabyluu@users.noreply.github.com>
2018-10-09 16:54:22 -0700

    Add files via upload

---
+++ #bucleFor.py
@@ -0,0 +1,9 @@
+#bucle For.py
+
+for x in range(10);
+	print(x)
+
+print(".............................")
+
+	for k in range(1,10);
+		print(k)
---
+++ whileLoop.py
@@ -0,0 +1,8 @@
+#whileLoop
+
+i = 0 
+while i < 100:
+
+#while True:
+	print("#",i)
+	i += 1
